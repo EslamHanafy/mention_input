c44be3cbad7fd2d745eab7bc13bea91d7c8ccfd5
deep-mb <119476986+deep-mb@users.noreply.github.com>
2023-12-22 16:14:46 +0530

    Update input_section.widget.dart

--- lib/widgets/input_section.widget.dart
+++ lib/widgets/input_section.widget.dart
@@ -85,7 +85,17 @@ class InputSection extends StatelessWidget {
             borderRadius: borderRadius ?? BorderRadius.circular(16),
             color: color ?? Colors.white,
           ),
-      child: Expanded(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Row(
+              children: [
+                if (!shouldHideLeftWidgets) ...?leftWidgets,
+                SizedBox(
+                  width: leftWidgets != null ? leftInputMargin : 0,
+                ),
+                
+                Expanded(
                   child: TextField(
                     minLines: minLines,
                     maxLines: maxLines,
@@ -108,6 +118,27 @@ class InputSection extends StatelessWidget {
                     ),
                   ),
                 ),
+                SizedBox(
+                  width: rightWidgets != null ? rightInputMargin : 0,
+                ),
+                if (!shouldHideRightWidgets) ...?rightWidgets,
+                shouldShowSendButton && hasSendButton
+                    ? const SizedBox(width: 8)
+                    : const SizedBox(),
+                shouldShowSendButton && hasSendButton
+                    ? IconButton(
+                        padding: EdgeInsets.zero,
+                        icon: sendIcon ?? const Icon(Icons.send),
+                        onPressed: () {
+                          onSend?.call();
+                          if (clearTextAfterSent) controller.clear();
+                        },
+                      )
+                    : const SizedBox(),
+              ],
+            ),
+        ],
+      ),
     );
   }
 }
