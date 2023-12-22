dc1df38dd6c5f4656b99ffc2242becb7c5858a90
deep-mb <119476986+deep-mb@users.noreply.github.com>
2023-12-22 13:59:50 +0530

    Update mention_input.controller.dart

--- lib/controllers/mention_input.controller.dart
+++ lib/controllers/mention_input.controller.dart
@@ -3,4 +3,12 @@ typedef GetTextMethod = String Function();
 class MentionInputController {
   late Function clearText;
   late GetTextMethod getText;
+  
+  String defaultText;
+
+   MentionInputController({this.defaultText = ''});
+
+  void setDefaultText(String text) {
+    defaultText = text;
+  }
 }
